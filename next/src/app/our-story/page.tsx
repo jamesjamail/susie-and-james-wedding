2ab2115b8aa0f82abd376575185ca8b4a6ebd60f
James Jamail <james@jamesjamail.com>
2025-02-17 14:05:02 -0600

    fix(our-story): replace content that copilot overwrote

--- next/src/app/our-story/page.tsx
+++ next/src/app/our-story/page.tsx
@@ -6,8 +6,26 @@ export default function OurStory() {
       <main className={styles.main}>
         <h1>Our Story</h1>
         <p>
-          Susie &amp; James met in college. It was love at first sight.
-          They&apos;ve been inseparable ever since.
+          Think back to the not-quite-post-COVID haze of 2021, when venturing
+          out in pants with a zipper felt both brave and reckless. It was during
+          one of these early attempts to conjure memories of socializing in
+          indoor public spaces that we first crossed paths, among a group of
+          mutual friends at a downtown show. My brain, preoccupied with trying
+          to regain the atrophied muscle of small talk-making, was admittedly
+          not quite ready to tackle the complexities of flirting.
+        </p>
+        <p>
+          Fast forward a year of sporadic run-ins and meme exchanges, and I
+          found myself housesitting for James&apos;s best friend. Suddenly,
+          James began appearing under the noble guise of &quot;helping out&quot;
+          with his friend&apos;s musical gear. Was it selfless? Maybe. Was it
+          charming? Enough for me to say yes to a sushi date. The rest, as they
+          say, is history.
+        </p>
+        <p>
+          10+ trips, 1 puppy, and 3 joy-filled years later, we couldn&apos;t be
+          more excited to formally merge our lives amidst the company of our
+          dearest friends and family.{" "}
         </p>
       </main>
     </div>
