cd97e133c9f6dbaec73b526c41a1228f1893fe99
James Jamail <james@jamesjamail.com>
2025-03-03 13:18:25 -0600

    fix(photos): hard code image paths instead of using fs

--- next/src/app/photos/page.tsx
+++ next/src/app/photos/page.tsx
@@ -1,28 +1,11 @@
-import path from "path";
-import fs from "fs";
 import PhotoLibrary from "@/components/PhotoLibrary/PhotoLibrary";
 
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs"; // Switch to Node.js runtime
-
 export default async function Photos() {
-  const imagesDirectory = path.join(
-    process.cwd(),
-    "public/images/photos-page-images"
-  );
-  const allFiles = fs.readdirSync(imagesDirectory);
-
-  const validImageExtensions = [".jpg", ".jpeg", ".png", ".gif"];
-  const imageFiles = allFiles.filter((file) => {
-    const extension = file.slice(file.lastIndexOf(".")).toLowerCase();
-    return validImageExtensions.includes(extension);
-  });
-
   return (
     <div>
       <main>
         <h1>Photos</h1>
-        <PhotoLibrary imageFiles={imageFiles} />
+        <PhotoLibrary />
       </main>
     </div>
   );
