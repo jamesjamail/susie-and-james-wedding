b4a2c1eda07a18276f27ea9104efb91b2960a47c
James Jamail <james@jamesjamail.com>
2025-08-02 14:24:11 -0500

    fix(photos): revert photos to working prod implementation, fix hamburger btn styling

--- next/src/components/PhotoLibrary/PhotoLibrary.tsx
+++ next/src/components/PhotoLibrary/PhotoLibrary.tsx
@@ -1,58 +1,45 @@
 "use client";
+import styles from "./PhotoLibrary.module.scss";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
-import { useState } from "react";
-import PhotoAlbum from "react-photo-album";
-import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
-import { Fullscreen } from "yet-another-react-lightbox/plugins";
-import "react-photo-album/rows.css";
-
-const photos = [
-  {
-    src: "/images/photos-page-images/CB8F5806-5D10-42CF-9B49-2F409041A588.JPG",
-    width: 4,
-    height: 3,
-  },
-  {
-    src: "/images/photos-page-images/E1D3E62B-0A18-406F-B947-7FFF5A97CC50.JPG",
-    width: 4,
-    height: 3,
-  },
-  {
-    src: "/images/photos-page-images/F5FE9C58-C9D8-4D1F-8FA5-A9269E8D15C9.JPG",
-    width: 4,
-    height: 3,
-  },
-  { src: "/images/photos-page-images/IMG_0322.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_1308.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_1665.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_2007.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_3175.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_3476.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_4243.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/IMG_7211.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/image000001.JPG", width: 4, height: 3 },
-  { src: "/images/photos-page-images/A_0042.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/SP_0013.jpg", width: 4, height: 3 },
-  { src: "/images/photos-page-images/SP_0017.jpg", width: 4, height: 3 },
+const imageFiles = [
+  "A_0042.jpg",
+  "SP_0013.jpg",
+  "SP_0017.jpg",
+  "CB8F5806-5D10-42CF-9B49-2F409041A588.JPG",
+  "E1D3E62B-0A18-406F-B947-7FFF5A97CC50.JPG",
+  "F5FE9C58-C9D8-4D1F-8FA5-A9269E8D15C9.JPG",
+  "IMG_0322.jpg",
+  "IMG_1308.jpg",
+  "IMG_1665.jpg",
+  "IMG_2007.jpg",
+  "IMG_3175.jpg",
+  "IMG_3476.jpg",
+  "IMG_4243.jpg",
+  "IMG_7211.jpg",
+  "image000001.JPG"
 ];
 
-const PhotoLibrary = () => {
-  const [index, setIndex] = useState<number | null>(null);
-
+const PhotoLibrary: React.FC = () => {
   return (
-    <>
-      <PhotoAlbum photos={photos} layout="rows" />
-      {index !== null && (
-        <Lightbox
-          slides={photos}
-          open
-          index={index}
-          close={() => setIndex(null)}
-          plugins={[Fullscreen]}
-        />
-      )}
-    </>
+    <PhotoProvider>
+      <div className={styles.grid}>
+        {imageFiles.map((file, index) => (
+          <div key={index} className={styles.gridItem}>
+            <PhotoView src={`/images/photos-page-images/${file}`}>
+              <img
+                src={`/images/photos-page-images/${file}`}
+                alt={`Photo ${index + 1}`}
+                width={300}
+                height={300}
+                className={styles.image}
+              />
+            </PhotoView>
+          </div>
+        ))}
+      </div>
+    </PhotoProvider>
   );
 };
 
