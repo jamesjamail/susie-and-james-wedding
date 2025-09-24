6a5f582d9316a7464095a90d15d1fd606e8a0dee
James Jamail <james@jamesjamail.com>
2025-09-24 10:57:41 -0500

    fix(rsvp): fix build errors

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import Header from "../components/Header/Header";
-import RSVPBanner from "../components/RSVPBanner/RSVPBanner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
